5621a383d8b5127eefeed582441c70d9e935a441
Larry-Noble <nortexnoble@gmail.com>
2024-06-17 08:45:40 -0700

    addpy

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10;
+number2 = 5;
+
+Addsum = number1 - number2;
+Subsum = number2 - number1;
+Multisum = number1 * number2;
+
+print("Addition" + number1 + "and" + number2 + "is" + Addsum\n"Subtraction" + number2 + "and" + number3 + "is" + Subsum\n"Multiplication" + number1 + "and" + number2 + "is" + Multisum );
